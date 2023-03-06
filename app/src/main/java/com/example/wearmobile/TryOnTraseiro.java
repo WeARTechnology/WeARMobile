a6087caa15828005c7d265ae55e68ca0c693ca6a
Andrey de Souza <andrey0404.as@hotmail.com>
2023-03-06 15:22:06 -0300

    Arrumando telas

---
+++ app/src/main/java/com/example/wearmobile/TryOnTraseiro.java
@@ -0,0 +1,14 @@
+package com.example.wearmobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TryOnTraseiro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_try_on_traseiro);
+    }
+}
