92478adb2571840f95e73104dd273d2c48b064ac
Polycarpo <105468470+Polycarpoo@users.noreply.github.com>
2023-04-13 17:01:38 -0300

    search and scrollview

--- app/src/main/java/com/example/wearmobile/Home.java
+++ app/src/main/java/com/example/wearmobile/Home.java
@@ -1,11 +1,15 @@
 package com.example.wearmobile;
 
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 
 public class Home extends AppCompatActivity {
 
@@ -13,6 +17,35 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.azul)));
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_search,menu);
+
+        MenuItem menuItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) menuItem.getActionView();
+        searchView.setQueryHint("Clique aqui para pesquisar");
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+
+
+
+                return false;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
 
     }
 }
